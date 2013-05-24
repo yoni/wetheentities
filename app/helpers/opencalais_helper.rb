52e12ef9bc4ef5297ed01dd672501cd0eaf2ef71
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-05-24 13:11:50 -0400

    Move the Open Calais license into a constant.

--- app/helpers/opencalais_helper.rb
+++ app/helpers/opencalais_helper.rb
@@ -1,11 +1,13 @@
 require 'calais'
 
 module OpencalaisHelper
+  LICENSE_ID = ENV['OPEN_CALAIS_LICENSE_ID']
+
   def enhance(content)
     Calais.enlighten(
         :content => content,
         :content_type => :raw,
-        :license_id => ENV['OPEN_CALAIS_LICENSE_ID']
+        :license_id => LICENSE_ID
     )
   end
   module_function :enhance
