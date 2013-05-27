171340af2e656437283da103b3d3061c0bfe516a
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-05-27 16:28:25 -0400

    Cache petition loading. This helps for local development.

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -5,17 +5,19 @@ class Petition
   attr_accessor :id
 
   def self.find(id)
-    petition = WeThePeople::Resources::Petition.find(id)
-    title_and_body = "#{petition.title}  #{petition.body}"
-    open_calais_result = OpenCalaisHelper.enhance title_and_body
-    semantria_result = SemantriaHelper.enhance({:id => petition.id, :text => title_and_body})
+    Rails.cache.fetch(id) do
+      petition = WeThePeople::Resources::Petition.find(id)
+      title_and_body = "#{petition.title}  #{petition.body}"
+      open_calais_result = OpenCalaisHelper.enhance title_and_body
+      semantria_result = SemantriaHelper.enhance({:id => petition.id, :text => title_and_body})
 
-    {
-        :id => petition.id,
-        :petition => petition,
-        :open_calais => open_calais_result,
-        :semantria => semantria_result
-    }
+      {
+          :id => petition.id,
+          :petition => petition,
+          :open_calais => open_calais_result,
+          :semantria => semantria_result
+      }
+    end
   end
 
 end
