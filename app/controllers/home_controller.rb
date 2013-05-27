e56d5bea1e47f779259f85d5614251f0d8a99fca
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-05-27 19:55:48 -0400

    More work on the homepage. Link to a few examples.
    Remove examples from the navigation, using only the homepage for now.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
     @petition_id = WeThePeopleHelper::EXAMPLE_PETITION_ID
     @example_path = petition_path(:id=> @petition_id, :only_path => false)
     @example_json_path = petition_path(:id=> @petition_id, :only_path => false, :format => 'json')
+    @petitions = WeThePeople::Resources::Petition.all.first(4)
   end
 end
