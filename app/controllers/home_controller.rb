b674a61efc0a644f7d81121f208323efff699589
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-05-27 20:04:40 -0400

    Link to a petition with location information.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,6 @@ class HomeController < ApplicationController
     @example_path = petition_path(:id=> @petition_id, :only_path => false)
     @example_json_path = petition_path(:id=> @petition_id, :only_path => false, :format => 'json')
     @petitions = WeThePeople::Resources::Petition.all.first(4)
+    @location_petition_example = WeThePeople::Resources::Petition.find(WeThePeopleHelper::TAIWAN_PETITION_ID)
   end
 end
