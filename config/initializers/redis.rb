121083a31408313a64356de257c8f6fb8dc835a9
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-05-30 17:02:50 -0400

    Fix REDIS URL.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,7 +3,7 @@ if ENV['TDDIUM']
   redis = YAML.load(ERB.new(File.read('config/redis.yml')).result)[Rails.env]
   REDIS = Redis.new(redis)
 else
-  redis_url = ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1/'
+  redis_url = ENV['REDISTOGO_URL'] || 'redis://127.0.0.1/'
   uri = URI.parse(redis_url)
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
