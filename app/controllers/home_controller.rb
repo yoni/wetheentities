1d9eddb48b5de418bb1307a5acc85397408d47d6
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-06-01 00:09:36 -0400

    Add more examples for collections.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,21 @@ class HomeController < ApplicationController
   caches_page :index
   def index
   end
+
   def api
     @petition_id = WeThePeopleHelper::EXAMPLE_PETITION_ID
-    @example_path = petition_path(:id=> @petition_id, :only_path => false)
-    @example_json_path = petition_path(:id=> @petition_id, :only_path => false, :format => 'json')
+    @example_petition_path = petition_path(:id=> @petition_id, :only_path => false)
+    @example_petition_json_path = petition_path(:id=> @petition_id, :only_path => false, :format => 'json')
+
+    @example_collection_path = petitions_path(:only_path => false)
+    @example_collection_json_path = petitions_path(:only_path => false)
+    @example_collection_issues_filter_path = clean_path(petitions_path(:issues => ['Health Care', 'Family'], :only_path => false))
+    @example_collection_statuses_filter_path = clean_path(petitions_path(:statuses => ['responded', 'pending response'], :only_path => false))
+    @example_collection_signatures_filter_path = clean_path(petitions_path(:signatures => 10000, :only_path => false))
+  end
+
+  private
+  def clean_path(path)
+    path.gsub('%5B', '[').gsub('%5D', ']')
   end
 end
--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -5,5 +5,11 @@ class HomeControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
-
+  test "should get api" do
+    get :api
+    assert_response :success
+    %w(issues statuses signatures).each do |filter|
+      assert_not_nil assigns("example_collection_#{filter}_filter_path".to_sym)
+    end
+  end
 end
