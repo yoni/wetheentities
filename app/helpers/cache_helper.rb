a22a9bd5bf127482715921207a081b6cea7a1f93
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-06-01 12:01:04 -0400

    Fix cache clearing regex.

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -1,6 +1,6 @@
 module CacheHelper
   def clear_collections_cache
-    REDIS.keys.select{|k| k =~ /Petition::COLLECTION_CACHE_PREFIX/}.each{|k| REDIS.del k}
+    REDIS.keys.select{|k| k =~ (Regexp.new Petition::COLLECTION_CACHE_PREFIX) }.each{|k| REDIS.del k}
   end
   module_function :clear_collections_cache
 end
