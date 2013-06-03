0dbb15f4aae1cc84838bad6e1806273bede3f9c5
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-06-03 11:54:26 -0400

    Add jsonp to Developer API documentation.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,9 +7,11 @@ class HomeController < ApplicationController
     @petition_id = WeThePeopleHelper::EXAMPLE_PETITION_ID
     @example_petition_path = petition_path(:id=> @petition_id, :only_path => false)
     @example_petition_json_path = petition_path(:id=> @petition_id, :only_path => false, :format => 'json')
+    @example_petition_jsonp_path = petition_path(:id=> @petition_id, :only_path => false, :format => 'js', :callback => 'jsonpCallback')
 
     @example_collection_path = petitions_path(:only_path => false)
     @example_collection_json_path = petitions_path(:only_path => false, :format => 'json')
+    @example_collection_jsonp_path = petitions_path(:only_path => false, :format => 'js', :callback => 'jsonpCallback')
     @example_collection_issues_filter_path = clean_path(petitions_path(:issues => ['Health Care', 'Family'], :only_path => false))
     @example_collection_statuses_filter_path = clean_path(petitions_path(:statuses => ['responded', 'pending response'], :only_path => false))
     @example_collection_signatures_filter_path = clean_path(petitions_path(:signatures => 10000, :only_path => false))
